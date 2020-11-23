236eea46cc7cbcb7e710c72632d32b35f893b436
timsheung <timsheung1995>
2020-11-23 16:42:47 +0800

    Update

---
+++ gomoku/evaluation.mjs
@@ -0,0 +1,3 @@
+export function getWinningSymbol(game) {
+    
+}
---
+++ gomoku/index.mjs
@@ -0,0 +1,5 @@
+import { getWinningSymbol } from "./evaluation.mjs";
+
+const dimension = 5;
+const arr = new Array(dimension).fill(new Array(dimension).fill("-"));
+
