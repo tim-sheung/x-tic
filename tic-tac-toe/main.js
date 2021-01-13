91ad3c8ee8f29fa910ffae5deae20222de53d4d1
timsheung <timsheung1995>
2021-01-13 17:21:17 +0800

    Update

---
+++ tic-tac-toe/main.js
@@ -0,0 +1,8 @@
+/**
+ * 0 = black
+ * 1 = white
+ */
+
+export function sum(a, b) {
+    return a + b;
+}
---
+++ tic-tac-toe/main.test.js
@@ -0,0 +1,5 @@
+import { sum } from "./main";
+
+test("test", () => {
+    expect(sum(1, 2)).toBe(3);
+});
