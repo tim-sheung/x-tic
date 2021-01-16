3d9a9a8114f84acada981d3f764d6d57e42e869f
tim.gnue <tim.gnue@gmail.com>
2021-01-16 18:46:25 +0800

    Update

--- tic-tac-toe/evaluation.js
+++ tic-tac-toe/evaluation.js
@@ -30,7 +30,22 @@ export function checkWinning(game, n) {
             return result;
         }
     }
-
+    // Check diagonals from top-left to bottom-right
+    const diagnoalsTLBR = getDiagonalsTLBR(game, n);
+    for (let i = 0; i < diagnoalsTLBR.length; i++) {
+        result = checkConsecutive(diagnoalsTLBR[i], n);
+        if (result !== 0) {
+            return result;
+        }
+    }
+    // Check diagonals from top-right to bottom-left
+    const diagnoalsTRBL = getDiagonalsTRBL(game, n);
+    for (let i = 0; i < diagnoalsTRBL.length; i++) {
+        result = checkConsecutive(diagnoalsTRBL[i], n);
+        if (result !== 0) {
+            return result;
+        }
+    }
     return 0;
 }
 
@@ -55,4 +70,48 @@ export function checkConsecutive(arr, n) {
         }
     }
     return 0;
+}
+
+// Get diagonals from top-Left to bottom-right
+export function getDiagonalsTLBR(game, n) {
+    const result = [];
+    // Extract the lower triangle of matrix
+    for (let i = 0; i <= game.length - n; i++) {
+        const arr = [];
+        for (let j = 0; j < game.length - i; j++) {
+            arr.push(game[i + j][j]);
+        }
+        result.push(arr);
+    }
+    // Extract the upper triangle of matrix, except the main diagonal
+    for (let j = 1; j <= game.length - n; j++) {
+        const arr = [];
+        for (let i = 0; i < game.length - j; i++) {
+            arr.push(game[i][i + j]);
+        }
+        result.push(arr);
+    }
+    return result;
+}
+
+// Get diagonals from top-right to bottom-Left
+export function getDiagonalsTRBL(game, n) {
+    const result = [];
+    // Extract the lower triangle of matrix
+    for (let i = 0; i <= game.length - n; i++) {
+        const arr = [];
+        for (let j = game.length - 1; j >= i; j--) {
+            arr.push(game[i + game.length - 1 - j][j]);
+        }
+        result.push(arr);
+    }
+    // Extract the upper triangle of matrix, except the main diagonal
+    for (let j = game.length - 2; j >= n - 1; j--) {
+        const arr = [];
+        for (let i = 0; i <= j; i++) {
+            arr.push(game[i][j - i]);
+        }
+        result.push(arr);
+    }
+    return result;
 }
--- tic-tac-toe/evaluation.test.js
+++ tic-tac-toe/evaluation.test.js
@@ -1,4 +1,4 @@
-import { checkConsecutive, checkWinning } from "./evaluation";
+import { checkConsecutive, checkWinning, getDiagonalsTLBR, getDiagonalsTRBL } from "./evaluation";
 
 test("checkConsecutive", () => {
     let arr = [1, 1, 1];
@@ -48,7 +48,7 @@ test("checkWinning", () => {
         });
     };
 
-    // horizontal
+    // Horizontal
     expect(checkWinning(arr, 3)).toBe(0);
     put(0, 0, 1);
     expect(checkWinning(arr, 3)).toBe(0);
@@ -74,7 +74,7 @@ test("checkWinning", () => {
     put(2, 2, 1);
     expect(checkWinning(arr, 3)).toBe(1);
 
-    // vertical
+    // Vertical
     reset();
     put(0, 0, 1);
     put(1, 0, 1);
@@ -103,4 +103,136 @@ test("checkWinning", () => {
     put(1, 0, 1);
     put(2, 0, 1);
     expect(checkWinning(arr, 3)).toBe(1);
+
+    // Diagonals - top-right to bottom-left
+    reset();
+    put(0, 0, 1);
+    put(1, 1, 1);
+    put(2, 2, 0);
+    expect(checkWinning(arr, 3)).toBe(0);
+    put(0, 2, 1);
+    put(2, 0, 1);
+    expect(checkWinning(arr, 3)).toBe(1);
+
+    // Diagonals - top-left to bottom-right
+    reset();
+    put(0, 2, 1);
+    put(1, 1, 1);
+    put(2, 2, 1);
+    expect(checkWinning(arr, 3)).toBe(0);
+    put(0, 0, 1);
+    expect(checkWinning(arr, 3)).toBe(1);
+    reset();
+    put(0, 2, 1);
+    put(1, 1, 0);
+    put(2, 2, 0);
+    expect(checkWinning(arr, 3)).toBe(0);
+    put(0, 0, 0);
+    expect(checkWinning(arr, 3)).toBe(-1);
+});
+
+test("getDiagonalsTLBR", () => {
+    let game = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+    ];
+    expect(getDiagonalsTLBR(game, 5)).toEqual([[1, 7, 13, 19, 25]]);
+    expect(getDiagonalsTLBR(game, 4)).toEqual([
+        [1, 7, 13, 19, 25],
+        [6, 12, 18, 24],
+        [2, 8, 14, 20],
+    ]);
+    expect(getDiagonalsTLBR(game, 3)).toEqual([
+        [1, 7, 13, 19, 25],
+        [6, 12, 18, 24],
+        [11, 17, 23],
+        [2, 8, 14, 20],
+        [3, 9, 15],
+    ]);
+    game = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ];
+    expect(getDiagonalsTLBR(game, 4)).toEqual([[1, 6, 11, 16]]);
+    expect(getDiagonalsTLBR(game, 3)).toEqual([
+        [1, 6, 11, 16],
+        [5, 10, 15],
+        [2, 7, 12],
+    ]);
+    expect(getDiagonalsTLBR(game, 2)).toEqual([
+        [1, 6, 11, 16],
+        [5, 10, 15],
+        [9, 14],
+        [2, 7, 12],
+        [3, 8],
+    ]);
+    game = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ];
+    expect(getDiagonalsTLBR(game, 3)).toEqual([[1, 5, 9]]);
+    expect(getDiagonalsTLBR(game, 2)).toEqual([
+        [1, 5, 9],
+        [4, 8],
+        [2, 6],
+    ]);
+});
+
+test("getDiagonalsTRBL", () => {
+    let game = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+    ];
+    expect(getDiagonalsTRBL(game, 5)).toEqual([[5, 9, 13, 17, 21]]);
+    expect(getDiagonalsTRBL(game, 4)).toEqual([
+        [5, 9, 13, 17, 21],
+        [10, 14, 18, 22],
+        [4, 8, 12, 16],
+    ]);
+    expect(getDiagonalsTRBL(game, 3)).toEqual([
+        [5, 9, 13, 17, 21],
+        [10, 14, 18, 22],
+        [15, 19, 23],
+        [4, 8, 12, 16],
+        [3, 7, 11],
+    ]);
+    game = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ];
+    expect(getDiagonalsTRBL(game, 4)).toEqual([[4, 7, 10, 13]]);
+    expect(getDiagonalsTRBL(game, 3)).toEqual([
+        [4, 7, 10, 13],
+        [8, 11, 14],
+        [3, 6, 9],
+    ]);
+    expect(getDiagonalsTRBL(game, 2)).toEqual([
+        [4, 7, 10, 13],
+        [8, 11, 14],
+        [12, 15],
+        [3, 6, 9],
+        [2, 5],
+    ]);
+    game = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ];
+    expect(getDiagonalsTRBL(game, 3)).toEqual([[3, 5, 7]]);
+    expect(getDiagonalsTRBL(game, 2)).toEqual([
+        [3, 5, 7],
+        [6, 8],
+        [2, 4],
+    ]);
 });
